20ae3139ceaa7be27db95cdc0d067cd2401ea7f9
Jayneshpatel-17 <pateljayneshk@gmail.com>
2026-02-10 23:06:09 +0530

    Different datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(10+3) we are using nodejs , not browser
+
+console.log(3
+    +
+3) // code readability should be high
+
+let name = "Jaynesh"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
